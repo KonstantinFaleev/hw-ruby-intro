c8490046fc2ac90d2aa64c1d9c132db78e66c59d
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-10-16 22:04:42 +0300

    update1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,16 +3,53 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
-
+  # Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
+  arramount=0
+  for x in arr
+    arramount+=x
+  end
+  return arramount
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  # Define a method max_2_sum(array) which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
+  if arr.size==0
+    return 0
+  elsif arr.size==1
+    return arr[0]
+  else
+    number1=arr[0]
+    number2=arr[1]
+    x=2
+    while x<arr.size
+      if(number1<number2)
+        if(arr[x]>number1)
+          number1=arr[x]
+        end
+      else
+        if(arr[x]>number2)
+          number2=arr[x]
+        end
+      end
+      x=x+1
+    end
+  end
+  return number1+number2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  # Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n. sum_to_n?([], n) should return false for any value of n, by definition.
+  for x in 0...arr.size
+    for y in 0...x
+      if(arr[x]+arr[y]==n)
+        return true
+      end
+    end
+  end
+  if(arr.size==0 and n==0)
+    return false
+  end
+  return false
 end
 
 # Part 2
